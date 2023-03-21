79b8a80dc85e269772cf1de251d0e744a5ec2539
e-bite <vtsinari@yahoo.com>
2023-03-21 23:28:15 +0200

    third

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ df = pd.read_csv("finance_liquor_sales.csv")
 print(df.columns)
 df1 = df.groupby(['zip_code', 'item_description'])['bottles_sold'].sum()
 df_popular = df1.groupby('zip_code').idxmax()
-df2 = df.groupby('store_number')['bottles_sold'].sum()
-total_sales = df2.sum()
+
+df2 = df.groupby(['store_number'])['sale_dollars'].sum()
+total_sales = df['sale_dollars'].sum()
 df_percentage = (df2 / total_sales) * 100
 df_popular.to_csv('popular.csv')
 df_percentage.to_csv('percentage.csv')
+
